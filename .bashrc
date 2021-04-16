cac8673f97d407da1eb96a790ad34c6a184e8f95
pengyan510 <pengyan510@gmail.com>
2021-04-16 05:26:34 +0000

    initial commit

---
+++ .bashrc
@@ -0,0 +1,13 @@
+# aliases
+alias gs="git status"
+alias ll="ls -lah"
+
+# colors
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+
+# prompt
+export PS1="${ORANGE}$USER${RESET}[${GREEN}\$PWD${RESET}]\n $ "
---
+++ setup.sh
@@ -0,0 +1,18 @@
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+delete_file_if_exists() {
+    if [ -f $1 ] || [ -L $1 ]
+    then
+	rm $1
+    elif [ -d $1 ]
+    then
+	rm -r $1
+    fi
+}
+
+delete_file_if_exists "$HOME/.bashrc"
+delete_file_if_exists "$HOME/.vimrc"
+delete_file_if_exists "$HOME/.vim"
+ln -s "$HERE/.bashrc" "$HOME/.bashrc"
+ln -s "$HERE/.vimrc" "$HOME/.vimrc"
+ln -s "$HERE/.vim" "$HOME/.vim"
